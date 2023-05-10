6d0f560dfcbf07208626c40e89662aca242427d0
Dorian Patterson <imaginationac@gmail.com>
2023-05-10 11:13:49 -0400

    Add create record action, toast message

--- force-app/main/default/aura/createNewAccounts/createNewAccountsController.js
+++ force-app/main/default/aura/createNewAccounts/createNewAccountsController.js
@@ -1,5 +1,18 @@
 ({
-    myAction : function(component, event, helper) {
-
+    // Clear the form, update the table. On success show a toast message.
+    // Add the created record ID to an array.
+    // Then use the LDS to iterate through the the array and add the record details.(?)
+    handleReset : function(component, event, helper){
+        component.find('field').forEach(function(f){
+            f.reset();
+        });
+    },
+    handleSuccess : function(compoment, event, helper){
+        compoment.find('notifLib').showToast({
+            "title": "Account successfully created!",
+            "message": event.getParam("message"),
+            "variant": "info"
+        });
     }
+
 })
