fd12c0a9567ea605da8c42cf1d8b1efe0d52b264
Dorian Patterson <imaginationac@gmail.com>
2023-05-10 12:02:02 -0400

    Add data table that updates with new data

--- force-app/main/default/aura/createNewAccounts/createNewAccountsController.js
+++ force-app/main/default/aura/createNewAccounts/createNewAccountsController.js
@@ -2,6 +2,13 @@
     // Clear the form, update the table. On success show a toast message.
     // Add the created record ID to an array.
     // Then use the LDS to iterate through the the array and add the record details.(?)
+    init : function (component, event, helper){
+        component.set('v.mycolumns', [
+            { label: 'Account Name', fieldName: 'Name', type: 'text'},
+            { label: 'Created Date', fieldName: 'CreatedDate', type: 'date'}
+        ]);
+        helper.getData(component);
+    },
     handleReset : function(component, event, helper){
         component.find('field').forEach(function(f){
             f.reset();
@@ -13,6 +20,8 @@
             "message": event.getParam("message"),
             "variant": "info"
         });
+        helper.getData(compoment);
+
     }
 
 })
